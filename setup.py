987ba6944a5dc1b55d271c9a044a3a66716ed93c
gopi1035 <gopi1035@gmail.com>
2022-10-19 23:50:03 +0530

    Updated

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
-REQUIREMENT_FILE_NAME = "requirement.TXT"
+REQUIREMENT_FILE_NAME = "requirement.txt"
 
 def get_requirement_list() -> List[str]:
     """
     This function is going to return list of requirement mention in requirement.txt
 
-    return This function ig going to return a list which contain name of the libraries mentioned in requirement.txt
+    return This function is going to return a list which contain name of the libraries mentioned in requirement.txt
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines() #.remove("-e .")
 
 Project_Name= "Housing-Predictor"
-version = "0.0.1"
+version = "0.0.3"
 AUTHOR = "GOPI PANDIT"
 DISCRIPTION = "This is a my first Maching Learing Project"
 PACKAGE = ["Housing"]
@@ -23,10 +23,14 @@ name = "Housing-Predictor",
 version = "0.0.1",
 author=AUTHOR,
 description= DISCRIPTION,
-packages= PACKAGE,
+packages= find_packages(),
 install_requires = get_requirement_list()
 
 )
+
+
+# if __name__""__main__":
+#     print(get_requirement_list())
 
 
 
